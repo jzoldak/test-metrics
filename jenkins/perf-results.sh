de4bc8e0584b02c0af4272d769b23a1c802d704f
Jay Zoldak <zoldak@edx.org>
2014-07-28 17:25:02 -0400

    Update python path

--- jenkins/perf-results.sh
+++ jenkins/perf-results.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# virtualenv venv
-# source ./venv/bin/activate
-# pip install -r requirements.txt
-
-python -m metrics.perf_stats "$@"
+PYTHONPATH=.; python -m metrics.perf_stats "$@"
